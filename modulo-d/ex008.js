88fe9003bd4b20dc55f0e6b17335ecce952c953b
NicolasChirazawa <nicolaschfernandes@gmail.com>
2023-06-17 12:21:33 -0300

    update de exercs

---
+++ modulo-d/ex008.js
@@ -0,0 +1,10 @@
+var vel = 50;
+
+console.log(`A velocidade do seu carro é ${vel} km por hora`);
+
+//Condição Simples!
+if(vel > 60){
+    console.log("Você ultrapassou a velocidade permitida, MULTADO");
+}
+
+console.log("Dirija sempre usando cinto de segurança");
